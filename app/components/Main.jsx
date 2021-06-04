07f061f3a79ac72b56dd9afbe06e082b787901cf
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-06-04 14:43:31 +0600

    completed at first step

---
+++ app/components/Main.jsx
@@ -0,0 +1,121 @@
+import React, { useState, useCallback } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+    ScrollView,
+} from 'react-native';
+import Note from './Notes';
+
+const Main = () => {
+    const [notes, setNotes] = useState([]);
+    const [inputVal, setInputVal] = useState('');
+
+    const addNote = useCallback(() => {
+        if (inputVal.length) {
+            const d = new Date();
+            const payload = {
+                date: `${d.getMonth()}/${d.getDate()}/${d.getFullYear()}`,
+                note: inputVal,
+            }            
+            setNotes([payload, ...notes]);
+            setInputVal('');
+        }
+    }, [notes, inputVal]);
+
+    const onDelete = useCallback((i) => () => {
+        notes.splice(i, 1);
+        setNotes([...notes]);
+    }, [notes]);
+    
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerText}>To Do's yea!</Text>
+            </View>
+            <ScrollView style={styles.scrollContainer}>
+                {notes.map((item, i) => (
+                    <Note key={i} data={item} onDelete={onDelete(i)} />
+                ))}
+            </ScrollView>
+            <View styles={styles.footer}>
+                <TextInput
+                    onChangeText={(userInput) => setInputVal(userInput)}
+                    value={inputVal}
+                    style={styles.textInput}
+                    placeholder="> add something rad..."
+                    placeholderTextColor="#eee"
+                    underlineColorAndroid="transparent"
+                >
+                </TextInput>
+            </View>
+            <TouchableOpacity onPress={addNote} style={styles.addButton}>
+                <Text style={styles.addButtonText}>+</Text>
+            </TouchableOpacity>
+        </View>
+
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        backgroundColor: '#3d3d3d',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottomWidth: 10,
+        borderBottomColor: '#ddd',
+        paddingTop: 20,
+    },
+    headerText: {
+        color: '#ffd700',
+        fontSize: 36,
+        padding: 26,
+        fontWeight: "500",
+    },
+    scrollContainer: {
+        flex: 1,
+        marginBottom: 100,
+    },
+    footer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 10,
+    },
+    textInput: {
+        alignSelf: 'stretch',
+        color: '#ffd700',
+        padding: 20,
+        backgroundColor: '#252525',
+        borderTopWidth: 2,
+        borderTopColor: '#edeede',
+        fontSize: 32,
+    },
+    addButton: {
+        position: 'absolute',
+        zIndex: 11,
+        right: 20,
+        bottom: 100,
+        backgroundColor: '#3d3d3d',
+        width: 80,
+        height: 80,
+        borderRadius: 50,
+        alignItems: 'center',
+        elevation: 8,
+    },
+    addButtonText: {
+        color: '#ffd700',
+        fontSize: 26,
+        fontWeight: "700",
+        top: 20,
+    },
+});
+
+
+export default Main;
